6e1a25f4983285d296f28c558f3d21315ba173a3
soso_alive <aongsingmarma247@gmail.com>
2026-05-21 09:34:38 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Button action function
+function showMessage() {
+  alert("Thank you for visiting SOSO TECH!");
+}
+
+// You can add more functions later
+// Example: Change background color automatically
+
+console.log("Website Loaded Successfully");
